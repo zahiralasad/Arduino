c55bbaacf939b72cb007912c9035e8654a8452de
Zahir Al-Asad <zahiralasad@yahoo.com>
2026-02-16 22:19:51 +0100

    test ongoing

--- esp01_testServer/esp01_testServer.ino
+++ esp01_testServer/esp01_testServer.ino
@@ -46,4 +46,73 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available())
+  {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.print(request);
+  client.flush();
+
+  // Match the request
+  int value = LOW;
+  if (request.indexOf("/RELAY=ON") != -1)
+  {
+    Serial.println("RELAY=ON");
+    digitalWrite(RELAY,LOW);
+    value = LOW;
+  }
+  if (request.indexOf("/RELAY=OFF") != -1)  
+  {
+    Serial.println("RELAY=OFF");
+    digitalWrite(RELAY,HIGH);
+    value = HIGH;
+  }
+  //Display the HTML web page
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  this is a must
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+  client.println("<title>ESP8266 RELAY Control</title>");
+  client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+  client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+  client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+  client.println(".button2 {background-color: #77878A;}</style>");
+  client.println("</head>");
+  client.println("<body>");
+  client.println("<body>");
+  client.println("<h1 align = center>ESP01 RELAY CONTROL</h1>");
+  client.print("<h2 align = center>RELAY STATUS: ");
+
+  if(value == HIGH) 
+  {
+    client.print("OFF");
+  } 
+  else 
+  {
+    client.print("ON");
+  }
+  client.println("</h2>");
+  client.println("<br>");
+  client.println("<a href=\"/RELAY=ON\"><button class=\"button\">TURN ON</button></a>");
+  client.println("<a href=\"/RELAY=OFF\"><button class=\"button button2\">TURN OFF</button></a><br>");
+  client.println("</body>");
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
 }
--- music_visualizer/led_master/led_master.ino
+++ music_visualizer/led_master/led_master.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
 
+#define READ_PIN A0        // external ADC only (or remove if unused)
+#define BUTTON_PIN 0       
+
+const char *ssid = "Tele2_48b021"; // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "hdmkvtnh";  // The password of the Wi-Fi network
+
+WiFiUDP UDP;
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  Serial.begin(115200);
+  delay(1000);
+  //Serial.println("Starting.....");
+  //Serial.println();
+  //Serial.print("Setting soft-AP ... ");
+
+  // Start Access Point
+  WiFi.mode(WIFI_AP);
+  WiFi.persistent(false);
+  Serial.print("AP IP: ");
+  WiFi.softAP(ssid, password);
+  Serial.print("Soft-AP IP address = "); //192.168.4.1
+  Serial.println(WiFi.softAPIP());
+
+  UDP.begin(7171);
+  Serial.println("UDP server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // ---- Button test ----
+  if(digitalRead(BUTTON_PIN) == LOW){
+    Serial.println("Button pressed");
+    delay(300);
+  }
+
+  // ---- Analog test ----
+#ifdef A0
+  int val = analogRead(A0);
+  Serial.print("Analog: ");
+  Serial.println(val);
+#endif
+
+  delay(500);
 }
